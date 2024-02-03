513d0e8617750d6c7167b92b86691c4deb767284
Zhang Yao <cineazhan@gmail.com>
2024-02-03 10:18:28 +0800

    初始化

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+//! This create is used for preserve this name before this project is finished developing.
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+
+    }
+}
